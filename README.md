bc7fab31069127869700ed91cf7597cf9df4ff43
Stefan <stefankopieczek@gmail.com>
2013-12-03 20:38:18 +0000

    Added navigation.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ automata
 ========
 
 Library for easily creating and visualising cellular automata, such as Conway's Game of Life.
+
+Preset automata:
+ - GameOfLife
+ - GameOfZombies (Game of life with less death by overcrowding, but more zombies!)
+ - GameOfAnts (Langton's Ant)
+ 
+ Controls:
+  - Pan using arrow keys
+  - Zoom using +/-
--- src/com/stefankopieczek/cellsheets/CellSheetCanvas.java
+++ src/com/stefankopieczek/cellsheets/CellSheetCanvas.java
@@ -3,10 +3,17 @@ package com.stefankopieczek.cellsheets;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 
 import com.stefankopieczek.cellsheets.CellSheet.Pair;
 
@@ -37,6 +44,8 @@ public class CellSheetCanvas extends JComponent
 		
 		setPreferredSize(new Dimension(320, 320));
 		setBackground(mColors.get(0));
+		
+		setKeyBindings();
 	}
 	
 	public synchronized void setCellSheet(CellSheet cellSheet)
@@ -119,5 +128,72 @@ public class CellSheetCanvas extends JComponent
 		double yCoord = y * mScale - mYOffset + height/2;		
 		g.fillRect((int)Math.floor(xCoord), (int)Math.floor(yCoord),
 				(int)Math.ceil(mScale), (int)Math.ceil(mScale));
+	}
+	
+	private void setKeyBindings()
+	{
+		KeyStroke MINUS_1 = KeyStroke.getKeyStroke("MINUS");
+		KeyStroke MINUS_2 = KeyStroke.getKeyStroke("SUBTRACT");
+		KeyStroke PLUS_1 = 
+				KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, 
+						               KeyEvent.SHIFT_DOWN_MASK);
+		KeyStroke PLUS_2 = KeyStroke.getKeyStroke("ADD");		
+		KeyStroke LEFT = KeyStroke.getKeyStroke("LEFT");
+		KeyStroke RIGHT = KeyStroke.getKeyStroke("RIGHT");
+		KeyStroke UP = KeyStroke.getKeyStroke("UP");
+		KeyStroke DOWN = KeyStroke.getKeyStroke("DOWN");
+		
+		InputMap inputMap = getInputMap();
+		inputMap.put(MINUS_1, "evt_zoom_out");
+		inputMap.put(MINUS_2, "evt_zoom_out");
+		inputMap.put(PLUS_1, "evt_zoom_in");
+		inputMap.put(PLUS_2, "evt_zoom_in");
+		inputMap.put(LEFT, "evt_pan_left");
+		inputMap.put(RIGHT, "evt_pan_right");
+		inputMap.put(UP, "evt_pan_up");
+		inputMap.put(DOWN, "evt_pan_down");
+		
+		ActionMap actionMap = getActionMap();
+		actionMap.put("evt_zoom_out", new ZoomAction(0.9));
+		actionMap.put("evt_zoom_in", new ZoomAction(1.2));
+		actionMap.put("evt_pan_left", new PanAction(-1, 0));
+		actionMap.put("evt_pan_right", new PanAction(1, 0));
+		actionMap.put("evt_pan_up", new PanAction(0, -1));
+		actionMap.put("evt_pan_down", new PanAction(0, 1));
+	}
+	
+	private class ZoomAction extends AbstractAction
+	{
+		private double mScaleScale;
+		
+		public ZoomAction(double scaleScale)
+		{
+			mScaleScale = scaleScale;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{			
+			mScale *= mScaleScale; 
+		}		
+	}
+	
+	private class PanAction extends AbstractAction
+	{
+		private int mXPan;
+		private int mYPan;
+		
+		public PanAction(int xPan, int yPan)
+		{
+			mXPan = xPan;
+			mYPan = yPan;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			mXOffset += mXPan;
+			mYOffset += mYPan;
+		}		
 	}
 }
